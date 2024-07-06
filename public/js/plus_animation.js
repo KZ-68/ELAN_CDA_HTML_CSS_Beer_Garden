60d3930bdc9d5a1ef51bc1b5b59619af23ab0dd3
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-07-06 22:28:41 +0200

    feat: add layer for services items

--- public/js/plus_animation.js
+++ public/js/plus_animation.js
@@ -1,7 +1,24 @@
 let servicesImagesWrapper = document.querySelectorAll(".services-images-wrapper");
 
 servicesImagesWrapper.forEach(serviceImageWrapper => {
-    serviceImageWrapper.addEventListener('focus', () => {
-        
-    })
+    let layer = document.createElement('div');
+    let addLayer = serviceImageWrapper.appendChild(layer);
+    serviceImageWrapper.insertBefore(addLayer, serviceImageWrapper.firstChild);
+    addLayer.style.backgroundColor = '#C89C5C';
+    addLayer.style.width = '100%';
+    addLayer.style.height = '100%';
+    addLayer.style.zIndex = '1';
+    addLayer.style.position = 'absolute';
+    addLayer.style.top = '0';
+    addLayer.style.display = 'none';
+
+    serviceImageWrapper.addEventListener('mouseover', () => {
+        layer.style.opacity = 0.5
+        layer.style.display = 'block'
+    });
+
+    serviceImageWrapper.addEventListener('mouseout', () => {
+        layer.style.opacity = 0;
+        layer.style.display = 'none'
+    });
 });
