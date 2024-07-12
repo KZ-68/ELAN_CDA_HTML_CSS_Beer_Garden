bd1aed24afcf2da78b57d04b1aa09a4a577d6626
KZ-68 <zitnik.kevin@gmail.com>
2024-07-12 14:10:59 +0200

    feat: add classlist for plus rotation animation

--- public/js/plus_animation.js
+++ public/js/plus_animation.js
@@ -5,27 +5,30 @@ servicesImagesWrapper.forEach(serviceImageWrapper => {
     let layer = document.createElement('div');
     let addLayer = serviceImageWrapper.appendChild(layer);
     serviceImageWrapper.insertBefore(addLayer, serviceImageWrapper.firstChild);
-    addLayer.style.backgroundColor = '#C89C5C';
-    addLayer.style.width = '100%';
-    addLayer.style.height = '100%';
-    addLayer.style.zIndex = '1';
-    addLayer.style.position = 'absolute';
-    addLayer.style.top = '0';
-    addLayer.style.display = 'none';
+    layer.style.backgroundColor = '#C89C5C';
+    layer.style.width = '100%';
+    layer.style.height = '100%';
+    layer.style.zIndex = '1';
+    layer.style.position = 'absolute';
+    layer.style.top = '0';
+    layer.style.display = 'none';
+    layer.classList.add(".smooth-transition");
 
     serviceImageWrapper.addEventListener('mouseover', () => {
         layer.style.opacity = 0.5
         layer.style.display = 'block'
         serviceImageWrapper.lastElementChild.classList.add("zoomed")
-        let plusThinIcon = serviceImageWrapper.querySelector("img[class=plus-thin]")
-        plusThinIcon.style.display = "block"
+        let plusThinIcon = serviceImageWrapper.querySelector("img[class='plus-thin']")
+        plusThinIcon.style.visibility = "visible"
+        plusThinIcon.classList.add("rotating-clockwise")
     });
 
     serviceImageWrapper.addEventListener('mouseout', () => {
         layer.style.opacity = 0;
-        layer.style.display = 'none'
+        layer.style.display = "none"
         serviceImageWrapper.lastElementChild.classList.remove("zoomed")
-        let plusThinIcon = serviceImageWrapper.querySelector("img[class=plus-thin]")
-        plusThinIcon.style.display = "none"
+        let plusThinIcon = serviceImageWrapper.querySelector("img[class='plus-thin rotating-clockwise']")
+        plusThinIcon.classList.remove("rotating-clockwise")
+        plusThinIcon.style.visibility = "hidden"
     });
 });
